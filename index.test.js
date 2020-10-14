46071a59301b35849b100d7d3a22affb47ff9e07
Hugs <13450737557@163.com>
2020-10-15 00:23:10 +0800

    chore: add test suite

--- index.test.js
+++ index.test.js
@@ -3,15 +3,25 @@ const postcss = require('postcss')
 const plugin = require('./')
 
 async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+  let result = await postcss([plugin(opts)]).process(input, { from: 'example.css' })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+
+it('transform px to rem only in media block', async () => {
+  await run(
+    `.pc { padding: 10px };
+      @media screen and (min-width: 900px) {
+        .mobile {
+          padding: 10px;
+        }
+      }`,
+    `.pc { padding: 10px };
+      @media screen and (min-width: 900px) {
+        .mobile {
+          padding: 0.5rem;
+        }
+      }`, { propList: ['padding'], rootValue: 20})
 })
-
-*/
